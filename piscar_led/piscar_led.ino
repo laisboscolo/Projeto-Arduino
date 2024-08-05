0cc7eb6caaa1ea85c2ce0517d0dd1a40a0ed053b
Lais Boscolo <costaboscololais2008@gmail.com>
2024-08-05 15:27:09 -0300

    Comentários de programa

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,13 +1,16 @@
+//c++
+//programa que faz a led piscar (prograva que envia sinal alto para a saida 4)
+
 void setup() {
   // put your setup code here, to run once:
-   pinMode(4, OUTPUT);
+pinMode(4, OUTPUT);// declaraçao da variavel e portal configurada pra saida  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(4, HIGH);
+digitalWrite(4, HIGH);// envia sinal alto
 delay (1000);
-digitalWrite(4, LOW);
-delay (1000);
+digitalWrite(4, LOW);// envia sinal baixo
+delay (1000);// espera 1 segundo
 
 }
